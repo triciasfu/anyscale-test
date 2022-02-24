3a70414700000b679b00d67e1abf65f16b7eaf9e
Tricia Fu <triciasfu@gmail.com>
2022-02-23 16:20:46 -0800

    Update job-test.py

--- job-test.py
+++ job-test.py
@@ -4,8 +4,8 @@ ray.init()
 print("Hello from the driver!")
 @ray.remote
 def say_hi():
-    printf("hello")
-    printf("1")
-    printf("2")
+    print("hello")
+    print("1")
+    print("2")
 
 ray.get([say_hi.remote() for _ in range(5)])
